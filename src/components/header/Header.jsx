c9e5213c010c7197ba9dd3cba0c4be00ade596f8
Sahajaddin4 <sahajaddinsk786@gmail.com>
2024-08-13 20:09:39 +0530

    Final product ready

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='header shadow-xl  top-0 py-5  text-center  w-full'>
+    <div className='header shadow-xl sticky bg-white top-0 py-5  text-center  w-full'>
         <h1 className='font-bold text-3xl uppercase'>Codehelp Blogs</h1>
     </div>
   )
