70462adc8d11e2059ce2f0067825864d22f5bb4f
Wasifkalimkalim <wasifnit2k19@gmail.com>
2024-08-13 21:00:19 +0530

    version 2 setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import { AppContext } from './appcontext_Api/AppContext'
 import Spinner from './components/spinner/Spinner';
+import { BrowserRouter } from 'react-router-dom'
+
 function App() {
-  
+
   const {fetchData,loader,posts, page, totalPages, pageChange}=useContext(AppContext);
 
   useEffect(()=>{
        fetchData();
   },[])
   return (
-    <>
+    <BrowserRouter>
      <div className="app flex flex-col">
          <Header/>
          <div className="mid w-[40rem] mx-auto">
@@ -23,7 +25,7 @@ function App() {
          </div>
          <Footer page={page} totalPages={totalPages} pageChange={pageChange}/>
      </div>
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/components/body/Card.jsx
+++ src/components/body/Card.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Card({ post }) {
   return (
     <div className="blog mb-4">
       <div className="title">
-        <h1 className="font-bold text-lg">{post.title}</h1>
+        <Link to={`/blog/${post.id}`} >
+          <h1 className="font-bold text-lg">{post.title}</h1>
+        </Link>
       </div>
 
       <div className="author">
-        <p className="italic">By {post.author} on <span className="underline  font-semibold">{post.category}</span></p>
+        <p className="italic">
+          By {post.author} on{" "}
+          <span className="underline  font-semibold">{post.category}</span>
+        </p>
       </div>
       <div className="date">
         <p>Posted On {post.date}</p>
@@ -18,11 +24,13 @@ function Card({ post }) {
         <p className="text-md">{post.content}</p>
       </div>
       <div className="gap-x-2">
-          {
-          post.tags.map((tag, index)=>{
-            return <span key={index} className="text-blue-700 underline px-1">#{tag}</span>
-          })
-          }
+        {post.tags.map((tag, index) => {
+          return (
+            <span key={index} className="text-blue-700 underline px-1">
+              #{tag}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
