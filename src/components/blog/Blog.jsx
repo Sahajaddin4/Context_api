a287770a9c77a8765c445bd490feae775bff7f61
Sahajaddin4 <sahajaddinsk786@gmail.com>
2024-08-13 23:00:25 +0530

    search by title done

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Header from "../header/Header";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Card from "../body/Card";
 import Spinner from "../spinner/Spinner";
 
 
 const Blog = () => {
-
+  
+  const navigate=useNavigate();
   const [posts, setPosts] = useState([]);
   const [loader, setLoader] = useState(false);
   const newBaseUrl = "https://codehelp-apis.vercel.app/api/get-blogs";
-
+  
 
   const {blogId} = useParams();
 
@@ -34,32 +35,45 @@ const Blog = () => {
     fetchBlogData()
   },[])
 
+  
   return (
-    <div>
+   
       <div className="app flex flex-col">
         <Header />
         {
             loader ? <Spinner/> :
             <div className="w-[40rem] mx-auto my-5">
         <button
-              className=" py-2 shadow-md rounded-md border border-gray-400 px-4"
-              
+              className=" py-2 shadow-md rounded-md border border-gray-400 mb-3 px-4"
+              onClick={()=>{
+                navigate('/')
+              }}
               >
               Back
             </button>
         {
             posts ?
-            <Card post={posts[0]}/> : <div>Not data found</div>
-            // posts?posts.map((post)=>{
-            //     return <Card post={post}key={post.id} />
-            // }):<div>No post found</div>
+            <Card post={posts.find((post)=>post.id===blogId)}/>
+            : <div>Not data found</div>
+            
          }
+         <div className="realted my-5">
+          <h1 className="font-bold text-3xl">Related data</h1>
+         </div>
+         <div className="related-posts">
+           {
+            posts?
+            posts.filter(deletePost=>deletePost.id!==blogId).map((post)=>{
+              return <Card post={post} key={post.id}/>
+            }):<div>No Data</div>
+           }
+         </div>
         </div>
         }
 
 
       </div>
-    </div>
+ 
   );
 };
 
--- src/components/body/Card.jsx
+++ src/components/body/Card.jsx
@@ -1,40 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Spinner from "../spinner/Spinner";
 
 function Card({ post }) {
-  console.log(post);
+  //console.log(post);
   return (
-    <div className="blog mb-4">
-      <div className="title">
-        <Link to={`/blog/${post.id}`} >
-          <h1 className="font-bold text-lg">{post.title}</h1>
-        </Link>
-      </div>
-
-      <div className="author">
-        <p className="italic">
-          By {post.author} on{" "}
-          <span className="underline  font-semibold">{post.category}</span>
-        </p>
-      </div>
-      <div className="date">
-        <p>Posted On {post.date}</p>
-      </div>
-
-      <div className="content mt-2">
-        <p className="text-md">{post.content}</p>
-      </div>
-      <div className="gap-x-2">
-        {post.tags.map((tag, index) => {
-          return (
-            <span key={index} className="text-blue-700 underline px-1">
-              #{tag}
-            </span>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
+     <>
+       {!post?
+           <Spinner/>:
+           (<div className="blog mb-4">
+            <div className="title">
+              <Link to={`/blog/${post.id}`} >
+                <h1 className="font-bold text-lg">{post.title}</h1>
+              </Link>
+            </div>
+      
+            <div className="author">
+              <p className="italic">
+                By {post.author} on{" "}
+                <span className="underline  font-semibold">{post.category}</span>
+              </p>
+            </div>
+            <div className="date">
+              <p>Posted On {post.date}</p>
+            </div>
+      
+            <div className="content mt-2">
+              <p className="text-md">{post.content}</p>
+            </div>
+            <div className="gap-x-2">
+              {post.tags.map((tag, index) => {
+                return (
+                  <span key={index} className="text-blue-700 underline px-1">
+                    #{tag}
+                  </span>
+                );
+              })}
+            </div>
+          </div>) 
+      }
+     </>
+  )
+};
 
 export default Card;
